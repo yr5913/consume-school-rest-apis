146e623079109081e6745c789d352fec82d20f99
yr5913 <yr5913@rit.edu>
2024-02-27 16:47:51 -0500

    Added WebClient to consume the save message api in the school web app

--- src/main/java/com/school/apis/config/ProjectConfiguration.java
+++ src/main/java/com/school/apis/config/ProjectConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunctions;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class ProjectConfiguration {
@@ -20,5 +22,13 @@ public class ProjectConfiguration {
                 new RestTemplateBuilder();
         return restTemplateBuilder.basicAuthentication
                 ("admin@eazyschool.com", "admin").build();
+    }
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .filter(ExchangeFilterFunctions.
+                        basicAuthentication("admin@eazyschool.com", "admin"))
+                .build();
     }
 }
--- src/main/java/com/school/apis/controller/ContactController.java
+++ src/main/java/com/school/apis/controller/ContactController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -19,6 +21,9 @@ public class ContactController {
     ContactProxy contactProxy;
     @Autowired
     RestTemplate restTemplate;
+
+    @Autowired
+    WebClient webClient;
 
     @GetMapping("/getMessages")
     public List<Contact> getMessages(@RequestParam("status") String status) {
@@ -35,5 +40,17 @@ public class ContactController {
         ResponseEntity<Response> responseEntity = restTemplate.exchange(uri, HttpMethod.POST,
                 httpEntity, Response.class);
         return responseEntity;
+    }
+
+    @PostMapping("/saveMessage")
+    public Mono<Response> saveMessage(@RequestBody Contact contact) {
+        String uri = "http://localhost:8080/api/contact/saveMsg";
+        return webClient
+                .post()
+                .uri(uri)
+                .header("invocationFrom", "WebClient")
+                .body(Mono.just(contact), Contact.class)
+                .retrieve()
+                .bodyToMono(Response.class);
     }
 }
